96f8771849ca440ea03d651c796cf3b3d5b7203a
LeeHyungGeun <lhg4031@gmail.com>
2020-01-24 13:57:17 +0900

    Add a Log in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import View from './view';
 import State from './state';
+import Log from './log';
 
 const state = new State();
+const log = new Log(state);
 const view = new View(state);
-window.state = state;
+
+if (process.env.NODE_ENV === 'development') {
+  window.state = state;
+}
+
 view.render();
