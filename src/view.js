40e9126f14830f46596e169355abf9dffec29144
LeeHyungGeun <lhg4031@gmail.com>
2020-01-24 14:49:45 +0900

    Add a style of console.log and console.warn

--- src/view.js
+++ src/view.js
@@ -6,35 +6,69 @@ class View {
 
   _constructor() {
     this.$wrap = document.createElement('div');
+    this.$root = document.createElement('div');
     this.$style = document.createElement('style');
+    this.$wrap.appendChild(this.$root);
     document.body.appendChild(this.$wrap);
     document.head.appendChild(this.$style);
+
+    this.setStyle();
   }
 
-  update() {
-    const queue = this.state.flush();
-    queue.map((item) => {
-      const $div = document.createElement('div');
-      $div.innerHTML = item.logs.join(' ') || '';
-      this.$wrap.appendChild($div);
-    });
-  }
-
-  render() {
+  setStyle() {
     this.$style.innerHTML = `
-      .uiConsole-wrap {
+      .ui-console-wrap {
+        font-size: 14px;
         position: fixed;
         width: 100%;
-        height: 300px;
+        height: 20%;
         bottom: 0;
         left: 0;
         right: 0;
         z-index: 999999999;
-        background: red;
+        color: #727579;
+        background: #242424;
+      }
+
+      .ui-console {
+        font-size: 2em;
+        height: 100%;
+        width: 100%;
         overflow-y: scroll;
       }
+
+      .ui-console-log {
+        padding: 0.2em 0.5em;
+      }
+
+      .ui-console-log.info {
+
+      }
+
+      .ui-console-log.warn {
+        width: 100%;
+        color: #74663C;
+        background: #322B08;
+      }
     `
-    this.$wrap.classList.add('uiConsole-wrap');
+    this.$root.classList.add('ui-console');
+    this.$wrap.classList.add('ui-console-wrap');
+  }
+
+  update() {
+    const queue = this.state.flush();
+    if (queue.length) {
+      queue.map((item) => {
+        const $div = document.createElement('div');
+        $div.innerHTML = item.logs.join(' ') || '';
+        $div.classList.add('ui-console-log', item.logType);
+        this.$root.appendChild($div);
+      });
+      this.$root.scrollTop = this.$root.scrollHeight - this.$root.clientHeight;
+    }
+  }
+
+  render() {
 
     setInterval(() => {
       this.update();
