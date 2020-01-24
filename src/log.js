f6136f4696462b6fc1cf31d245c5be19f12d66d0
LeeHyungGeun <lhg4031@gmail.com>
2020-01-24 13:56:42 +0900

    Add a Log

---
+++ src/log.js
@@ -0,0 +1,35 @@
+class Log {
+  constructor(state) {
+    this.state = state;
+    this.console = {};
+    this.monkeyPatching();
+  }
+
+  monkeyPatching() {
+    const that = this;
+    const methods = ['log', 'info', 'warn', 'debug', 'error'];
+
+    if (!window.console) {
+      window.console = {}
+    } else {
+      methods.map((method) => {
+        that.console[method] = window.console[method];
+      });
+      that.console.time = window.console.time;
+      that.console.timeEnd = window.console.timeEnd;
+      that.console.clear = window.console.clear;
+    }
+
+    methods.map((method) => {
+      window.console[method] = (...args) => {
+        this.state.queue.push({
+          logType: method,
+          logs: args
+        });
+        that.console[method](...args);
+      };
+    });
+  }
+}
+
+export default Log;
